a14905c8bb31ded6f15766ab17c0300782329f11
bryancandi <quakeaz@gmail.com>
2025-10-27 00:05:42 -0700

    Added a trim function

--- bcsh.c
+++ bcsh.c
@@ -1,8 +1,11 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define MAX_LENGTH 1024
+
+void trim(char *line);
 
 int main(void)
 {
@@ -15,7 +18,9 @@ int main(void)
             break;
         }
 
-        if (strcmp(line, "exit\n") == 0)
+        trim(line);
+
+        if (strcmp(line, "exit") == 0)
         {
             break;
         }
@@ -27,4 +32,33 @@ int main(void)
     }
 
     return 0;
+}
+
+void trim(char *line)
+{
+    int start_index = 0;
+    int end_index = strlen(line) - 1;
+
+    if (line == NULL)
+    {
+        return;
+    }
+
+    while (isspace(line[start_index]))
+    {
+        start_index++;
+    }
+
+    while (end_index >= start_index && isspace(line[end_index]))
+    {
+        end_index--;
+    }
+
+    int i = 0;
+    while (start_index + i <= end_index) {
+        line[i] = line[start_index + i];
+        i++;
+    }
+
+    line[i] = '\0';
 }
